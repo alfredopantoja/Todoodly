eadef32ea42e9128b5075f2555e99a2b82559efe
dobabeswe <chololoy@gmail.com>
2012-12-30 08:30:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoodly::Application.config.secret_token = '3b3eae7ebab25245b5a30d1c05cb1739d5e9798a804f3695d08e911b06d89982b7919198f923fa868cdb8aac18b1cfa589ac518b9e06d164eb92a2518d9edc73'
