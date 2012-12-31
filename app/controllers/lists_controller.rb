4a1f0417c5d408e23eac6c93df041edb8ed1c938
dobabeswe <chololoy@gmail.com>
2012-12-30 20:04:30 -0600

    Create List new, create, and show actions and corresponding views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,23 @@ class ListsController < ApplicationController
 		@lists = List.all
 	end
 	
+	def new
+		@list = List.new
+	end
+
+	def create 
+		@list = List.new(params[:list])
+		if @list.save
+			flash[:notice] = "List created."
+			redirect_to list_url(@list)
+		else
+			flash[:error] = "Could not create list"
+			redirect_to new_list_url
+		end
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+	
 end
