f06335730ea63d008e5e2e604246f754763fc27d
dobabeswe <chololoy@gmail.com>
2013-01-03 19:28:23 -0600

    Create Tasks create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+
+	def create 
+		@list = List.find(params[:list_id])
+		@task = @list.tasks.new(params[:task])
+		if @task.save
+			flash[:notice] = "Task created"
+			redirect_to list_url(@list)
+		else
+			flash[:error] = "Could not add task at this time."
+			redirect_to list_url(@list)
+		end
+	end
+	
 end
